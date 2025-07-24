40e7ae0a90e26e7af264edd33b8d9020a44b8bc8
Sadeed <sadeed@Sadeeds-MacBook-Air.local>
2025-07-24 14:40:42 +0500

    added a progess bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { Play, Pause, SkipBack, SkipForward, VolumeX, Volume1, Volume2 } from '@lucide/svelte';
+  import { Play, Pause, SkipBack, SkipForward, FastForward, VolumeX, Volume1, Volume2 } from '@lucide/svelte';
   const playlist = [
 		{
 			artist: 'Twenty One Pilots',
@@ -24,7 +24,38 @@
   let isPlaying = false;
   let trackIndex = 0;
   let audioFile = new Audio(playlist[trackIndex].audio);
-  let volume = 50;
+  let volume = 5;
+  let trackDuration;
+  let currentTrackDuration;
+  let totalTrackDuration;
+  let progress = 0;
+
+  audioFile.onloadedmetadata = () => {
+    trackDuration = audioFile.duration;
+    updateTime();
+  }
+
+  audioFile.ontimeupdate = () => {
+    updateTime();
+
+    if (audioFile.ended) {
+      nextTrack();
+    }
+  }
+
+  const updateTime = () => {
+    // progress = (audioFile.currentTime / trackDuration) * 100;
+    progress = audioFile.currentTime * (100  / trackDuration);
+
+    let durMins = Math.floor((trackDuration % 3600) / 60);
+    let durSecs = Math.floor(trackDuration % 60);
+
+    let currMins = Math.floor((audioFile.currentTime % 3600) / 60);
+    let currSecs = Math.floor(audioFile.currentTime % 60).toString().padStart(2, '0');
+
+    totalTrackDuration = `${durMins}:${durSecs}`;
+    currentTrackDuration = `${currMins}:${currSecs}`;
+  }
 
   const playTrack = () => {
     if (isPlaying) {
@@ -50,6 +81,16 @@
     if (isPlaying) {
       audioFile.play();
     }
+  }
+
+  const seekForward = () => {
+    audioFile.currentTime += 10; // Seek forward by 10 seconds
+    updateTime();
+  }
+
+  const seekBackward = () => {
+    audioFile.currentTime -= 10; // Seek backward by 10 seconds
+    updateTime();
   }
 
   addEventListener ('keydown', (event) => {
@@ -78,10 +119,27 @@
   <img class="track-cover" src="{playlist[trackIndex].cover}" alt="track cover" />
   <h1>{playlist[trackIndex].name}</h1>
   <h2>{playlist[trackIndex].artist}</h2>
+
+  <div class="time-info">
+    <span class="progress-time">{currentTrackDuration}</span>
+
+    <div class="progress-bar">
+      <span class="bar" style="width: {progress}%;"></span>
+    </div>
+
+    <span class="track-duration">{totalTrackDuration}</span>
+  </div>
+  
   <section>
+    <button onclick={seekBackward}>
+      <!-- could not find a rewind icon lol -->
+      <FastForward style="transform: rotate(180deg);" />
+    </button>
+
     <button onclick={prevTrack}>
       <SkipBack/>
     </button>
+
     <button onclick= {playTrack}>
       {#if isPlaying}
         <Pause/>
@@ -89,10 +147,17 @@
         <Play/>
       {/if}
     </button>
+
     <button onclick={nextTrack}>
       <SkipForward/>
     </button>
+
+    <button onclick={seekForward}>
+      <FastForward/>
+    </button>
+
   </section>
+
   <div class="slider-container">
     <label for="volume-slider">
       {#if audioFile.volume === 0}
@@ -103,6 +168,7 @@
         <Volume2/>
       {/if}
     </label>
+
     <input name="volume-slider" type="range" min="0" max="100" bind:value={volume}
           oninput={() => {
             audioFile.volume = volume / 100;
@@ -110,6 +176,7 @@
     />
     <label for="volume-slider">{volume}</label>
   </div>
+
   <div class="playlist">
     <h3>Playlist</h3>
     <div class="tracks">
@@ -192,5 +259,41 @@
     border-radius: 10px;
     object-fit: cover;
     margin-bottom: 1rem;
+  }
+
+  .time-info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    width: 25%;
+    margin-top: 1.5rem;
+  }
+
+  .time-info span {
+    width: 50px;
+  }
+
+  .progress-bar {
+    width: 100%;
+    height: 5px;
+    background-color: #555;
+    border-radius: 2.5px;
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+  }
+
+  .progress-bar .bar {
+    position: absolute;
+    width: 30%;
+    height: 100%;
+    background-color: #f3f3f3;
+    border-radius: 2.5px;
+    transition: width 0.1s ease-in-out;
+  }
+
+  .progress-bar:hover .bar {
+    background-color:salmon ;
   }
 </style>
